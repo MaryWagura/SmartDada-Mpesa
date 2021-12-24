7a2a68a6b7e5dc51ffe895dc1a204c249d0f1ec1
MWagura <waguramarie@gmail.com>
2021-12-24 18:24:30 +0300

    first commit

---
+++ Pay.php
@@ -0,0 +1,41 @@
+<?php
+require_once('Lipa-Mpesa.php');
+//echo "bhjnmk";
+
+if (isset($_POST['submitReg'])) {
+
+
+	$telNum=$_POST['phonenumber'];
+	$amount=1;
+	$accRef=$_POST['SmartDada'];
+	echo "string";
+	 $access_token = accessTokenGenerator();
+	 mpesaSendMoney($telNum, $amount, $accRef, $access_token );
+	
+
+
+
+ 	$push = array('telNum' => $telNum,'Amount'=>$amount ,'accRef'=>$licenseNum);
+    print_r($push);
+   $arr=json_encode($push);
+
+	
+
+ 	$url="./LNM/stkPush.php";
+ 	$curl = curl_init();
+ 			curl_setopt($curl, CURLOPT_URL, $url);
+ 			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+ 				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+ 			  curl_setopt($curl, CURLOPT_POST, true);
+ 			  curl_setopt($curl, CURLOPT_POSTFIELDS, $arr);
+
+ 			  $curl_response = curl_exec($curl);
+
+ 			  print_r($curl_response);
+ 			  echo $curl_response;
+ }
+
+
+?>
+
