787feec3839ce2a0146e8d6383b961a04a6054ce
MWagura <waguramarie@gmail.com>
2021-12-24 19:23:50 +0300

    index file

--- index.php
+++ index.php
@@ -1,261 +1,134 @@
-<!DOCTYPE html>
-<html lang="en">
-<title>Smart Dada</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="AboutPage.css"> 
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-.fa-anchor,.fa-coffee {font-size:200px}
+<!-- <?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dogregistrationke";
+$_SESSION['DogLicenseNumber'];
+$DogLicenseNumber="DogLicenseNumber";
+$FirstName=$_SESSION['FirstName'];
+$IDNumber=$_SESSION['IDNumber'];
+$OwnerID=$_SESSION["OwnerID"];
 
-</style>
-<body>
-<div class="column">
-<!-- Navbar -->
-<div class="w3-top">
-  <div class="w3-bar w3-pink w3-card w3-left-align w3-large">
-    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white"> About Smart Dada</a>
-    
-  </div>
+$conn= new mysqli($servername, $username, $password,$dbname);
+    $result=$conn->query("SELECT * FROM ownerinformation where FirstName='$FirstName' and IDNumber ='$IDNumber'") or die("Failed to query DB".mysql_error());
+ $row= mysqli_fetch_array($result);
+ ?>
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dogregistrationke";
+$DogLicenseNumber=$_SESSION["DogLicenseNumber"];
 
-</div>
+  $connection= new mysqli($servername, $username, $password,$dbname);
+ $res=$connection->query("SELECT * FROM petdetails where DogLicenseNumber='$DogLicenseNumber' ") or die("Failed to query DB".mysql_error());
+ $row= mysqli_fetch_array($res);
+   $_SESSION['DogLicenseNumber'] = $row["DogLicenseNumber"]."<br><br>";
+ ?> -->
+ <!DOCTYPE html>
+ <html>
+ <head>
+  <title>Donation</title>
+ </head>
+ <body>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Payments.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-<!-- Header -->
-<header class="w3-container w3-white w3-center"  style="padding:128px 16px">
-<style>
-* {box-sizing: border-box;}
-body {font-family: Verdana, sans-serif;}
-.mySlides {display: none;}
-img {vertical-align: middle;}
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="Styles/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="Styles/bootstrap-4.1.0.min.js" type="text/javascript"></script>
+        <link href="Styles/bootstrap-4.1.0.min.css" rel="stylesheet" type="text/css"/>
+        <link href="Styles/MainStyle.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <?php
+      
+        ?>
+          <div class="w3-bar w3-pink w3-card w3-left-align w3-large">
+    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Donation - Smart Dada</a>
+                       </div>
+                       <center>
+        <div class="container register">
+            <center>
+            <div class="row" >
+                <div class="col-md-12">
+                    <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+                           
+                    </ul>
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active text-align form-new" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <h5 class="register-heading"><i>Mpesa Donation Form</h5></i>
+                            <div class="row register-form">
+                                <div class="col-md-12">
+                                    <form method="post">
+                                       
+         <p><b>Amount to Pay: 500</p></b>
+         <label for="phonenumber"><b>Input Your Phone Number</label><br><br></b>
+        <input type="text" class="form-control" name="phonenumber" placeholder="07" required><br><br> 
+                                            
+                                        <div class="form-group">
+                                             <a href="Pay.php">
+                                           <input type="button"  class="btnContactSubmit" value="Pay">
+                                            </a></div>
+                                          
+                                           </div> 
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade show text-align form-new" id="profile" role="tabpanel" aria-labelledby="profile-tab">
 
-/* Slideshow container */
-.slideshow-container {
-  max-width: 500px;
-
-  position: relative;
-  margin: auto;
-}
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  padding: 16px;
-  margin-top: -22px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-/* Position the "next button" to the right */
-.prev {
-  left: 0;
-  border-radius: 3px 0 0 3px;
-  }
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover {
-  background-color: rgba(0,0,0,0.8);
-}
- .next:hover {
-  background-color: rgba(0,0,0,0.8);
-  }
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 100px) {
-  .text {font-size: 11px}
-}
-</style>
-<body>
-
-
-
-<div class="slideshow-container">
-
-<div class="mySlides fade">
-  <img src="donated.jpg" style="width:100%">
- 
-</div>
-
-<div class="mySlides fade">
-  <img src="pads.jpg" style="width:100%">
-
-</div>
-
-<div class="mySlides fade">
-  <img src="hygiene.jpg" style="width:100%">
-  
-</div>
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-</div>
-<br>
-
-<div style="text-align:center">
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-</div>
-
-</header>
-
-<!-- First Grid -->
-<div class="w3-row-padding w3-pink w3-padding-64 w3-container">
-  <div class="w3-content">
-    <div class="w3-half w3-center">
-      <h1>About</h1>
-      <h15 class="w3-padding-20">There is no magic bullet solution to girls’ education, safety, and empowerment. These are complex issues requiring complex, holistic solutions.
- 
-Smart Dada addresses these challenges by equipping adolescent girls in Kenya with the tools they need to safely navigate puberty and unlock their potential. 
-
-These tools include sanitary pads and rights-based reproductive health education, delivered in the form of social and behavior change content (SBCC) through comics and magazines delivered to delivered to both girls and boys through schools and community organizations.</p></h15>
-    </div>
-
-    <div class="w3-half w3-center">
-      <img src="girls.jpg">
-    
-    </div>
-  </div>
-</div>
-
-<!-- Second Grid -->
- <div class="row w3-white w3-padding-64 w3-container">
-  <h3><i>What We Do:</i></h3>
-   <div class="w3-third w3-container">
-     <img src="periods.jpg">
-   </div>
-    <div class="w3-third w3-container">
-    <h2>Period Tracker</h2>
-    <p>Keep track of the menstrual cycle</p>
-  </div>
-   <div class="w3-third w3-container">
-     <img src="hygiene.jpg">
-    <h2>Menstrual Hygiene</h2>
-    <p>Provide information on how to wear and dispose pads.</p>
-  </div>
-    <div class="w3-third w3-container">
-      <img src="donated.jpg" width="200" height="150">
-    <h2>Donation</h2>
-    <p>Donation of sanitary pads.</p>
-  </div>
-</div> 
-<div class="w3-container w3-purple w3-center w3-opacity w3-padding-64">
-    <h1 class="w3-margin w3-xlarge">HELP GIRLS STEP INTO THEIR POTENTIAL. DONATE TODAY</h1>
-</div>
-
-<!-- Footer -->
-<footer class="w3-container w3-padding-64 w3-center w3-opacity">  
-  <div class="w3-xlarge w3-padding-32">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
- </div>
- <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">Smart Dada</a></p>
-</footer>
-</div>
-
-<script>
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-  var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
-</script>
-<script>
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
-</script>
-
-</body>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
+  
+
+  <div id="display">
+    <link rel="stylesheet" type="text/css" href="Payment.css">
+    
+
+  
+   <div id="display2">
+  
+
+       
+
+       
+       
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+    </div>
+  </div>
+  </form>
+</div>
+</label>
+
+</form> 
+</form>
+</center>
+</center>
+
+ 
+ </body>
+ </html>
