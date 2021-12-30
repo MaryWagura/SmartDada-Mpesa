8e374817be13864e7c21e10e97e1f2507407d9fa
MWagura <waguramarie@gmail.com>
2021-12-30 15:31:50 +0300

    new changes

--- callback_lnm.php
+++ callback_lnm.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 $dbname = "smartdada-mpesa";
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $db);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 //check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
