759dd9fe01884ecdecf1a9c48784651ee6dee5d9
MWagura <waguramarie@gmail.com>
2022-01-22 17:55:39 +0300

    new changes today

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -4,7 +4,7 @@
  use Kreait\Firebase\Factory;
  use Kreait\Firebase\ServiceAccount;
 
-$serviceAccount= ServiceAccount::fromJsonFile(__DIR__.'/smartdada-36cf3-firebase-adminsdk-z3bh9-3a8fc689fa.json')
+$serviceAccount= ServiceAccount::fromJsonFile(__DIR__.'/smartdada-36cf3-firebase-adminsdk-z3bh9-3a8fc689fa.json');
 
 
  $firebase= (new Factory)
