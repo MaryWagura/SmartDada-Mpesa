1e78d05934ded1f72a5fb729b05686942ad6f9cf
MWagura <waguramarie@gmail.com>
2022-01-22 17:35:52 +0300

    new changes today

---
+++ includes/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b44f75d40d616fe8f10c86adea24111078d7bbb3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b44f75d40d616fe8f10c86adea24111078d7bbb3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
