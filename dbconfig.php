19f432714d56cd147cd5c2ddbfe5a554c9f5a246
MWagura <waguramarie@gmail.com>
2022-01-24 13:25:36 +0300

    today

--- dbconfig.php
+++ dbconfig.php
@@ -9,9 +9,9 @@
 
  $factory= (new Factory)
 
-   ->withServiceAccount(__DIR__.'/smartdada-36cf3-firebase-adminsdk-z3bh9-3a8fc689fa.json');
+   ->withServiceAccount(__DIR__.'/smartdada-36cf3-firebase-adminsdk-z3bh9-3a8fc689fa.json')
    ->withDatabaseUrl('https://smartdada-36cf3-default-rtdb.firebaseio.com/');
-   ->create();
+  // ->create();
 
    $database = $factory->createDatabase();
 
