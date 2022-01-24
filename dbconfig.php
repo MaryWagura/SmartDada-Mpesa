6b64b9213a67dd6aed7943d008faa27c0fdb771e
MWagura <waguramarie@gmail.com>
2022-01-24 13:35:24 +0300

    finallly!

--- dbconfig.php
+++ dbconfig.php
@@ -4,15 +4,15 @@
  use Kreait\Firebase\Factory;
  use Kreait\Firebase\ServiceAccount;
 
-//$serviceAccount= ServiceAccount::fromJsonFile(__DIR__.'/smartdada-36cf3-firebase-adminsdk-z3bh9-3a8fc689fa.json')
+$serviceAccount= ServiceAccount::fromJsonFile(__DIR__.'/smartdada-36cf3-firebase-adminsdk-z3bh9-3a8fc689fa.json');
 
 
- $factory= (new Factory)
+ $firebase= (new Factory)
 
    ->withServiceAccount(__DIR__.'/smartdada-36cf3-firebase-adminsdk-z3bh9-3a8fc689fa.json')
    ->withDatabaseUrl('https://smartdada-36cf3-default-rtdb.firebaseio.com/')
    ->create();
 
-   $database = $factory->createDatabase();
+   $database = $firebase->createDatabase();
 
 ?>
